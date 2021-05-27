d8385dec74bce0c55e22cabb74b35af4e500cddb
Mario Viapiano <marioeviapiano@gmail.com>
2021-05-27 21:16:13 +0200

    create endpoint

--- server/spec/requests/create_endpoint_spec.rb
+++ server/spec/requests/create_endpoint_spec.rb
@@ -3,8 +3,8 @@
 require 'rails_helper'
 
 describe 'Create Endpoint' do
-  context 'when condition' do
-    it 'creates a new endpoint' do
+  context 'with valid params' do
+    it 'creates a new endpoint and return it serialized' do
       headers = {
         'Content-Type': 'application/vnd.api+json',
         'Accept': 'application/vnd.api+json'
@@ -18,35 +18,83 @@ describe 'Create Endpoint' do
             "response": {
               "code": 200,
               "headers": {},
-              "body": '"{ "message": "Hello, world" }"'
+              "body": '{ "message": "Hello, world" }'
             }
           }
         }
       }.to_json
       expected_response = {
+        "type": 'endpoints',
+        "attributes": {
+          "verb": 'GET',
+          "path": '/greeting',
+          "response": {
+            "code": 200,
+            "headers": {},
+            "body": '{ "message": "Hello, world" }'
+          }
+        }
+      }.as_json
+
+      action = -> { post '/endpoints', params: request, headers: headers }
+
+      expect(action).to change(Endpoint, :count).by(1)
+      expect(response).to have_http_status(201)
+      expect(json['data']).to include expected_response
+      expect(response.content_type).to eq 'application/vnd.api+json'
+    end
+  end
+
+  context 'with invalid params' do
+    it 'does not create an endpoint and return errors' do
+      create(:endpoint, verb: 'GET', path: '/greeting')
+      headers = {
+        'Content-Type': 'application/vnd.api+json',
+        'Accept': 'application/vnd.api+json'
+      }
+      params = {
         "data": {
           "type": 'endpoints',
-          "id": '12345',
           "attributes": {
             "verb": 'GET',
             "path": '/greeting',
             "response": {
               "code": 200,
-              "headers": {},
-              "body": '"{ "message": "Hello, world" }"'
+              "headers": "foo: 'bar'",
+              "body": '{ "message": "Hello, world" }'
             }
           }
         }
-      }
-      expected_headers = {
-        "Content-Type": 'application/vnd.api+json'
-      }
+      }.to_json
+      expected_response = {
+        "errors": [
+          {
+            'code': '100',
+            'detail': 'path - has already been taken',
+            'source': {
+              'pointer': '/data/attributes/path'
+            },
+            'status': '422',
+            'title': 'has already been taken'
+          },
+          {
+            "code": '100',
+            "detail": 'response - invalid format',
+            "source": {
+              "pointer": '/data/attributes/response'
+            },
+            "status": '422',
+            "title": 'invalid format'
+          }
+        ],
+      }.as_json
 
-      post '/endpoints', params: request, headers: headers
+      action = -> { post '/endpoints', params: params, headers: headers }
 
-      # expect(response).to have_http_status(200)
+      expect(action).not_to change(Endpoint, :count)
+      expect(response).to have_http_status(422)
       expect(json).to eq expected_response
-      expect(response.headers).to eq expected_headers
+      expect(response.content_type).to eq 'application/vnd.api+json'
     end
   end
 end
